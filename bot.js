e7f643b16f7ee3c43f345588113417149bec7c54
master-jay <41198521+master-jay@users.noreply.github.com>
2018-07-13 17:01:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NDY2NTUzOTk2NTA5OTcwNDMy.DipGuw.gn_IYCKJ8GEqb2F8v3jn9K_19n0);
